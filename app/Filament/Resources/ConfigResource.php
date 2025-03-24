7dfa60f11361f00128302ecb65110dbfe7f5d7c1
andreyfrost <flamedeluxe@yandex.ru>
2025-03-25 04:02:34 +0800

    Config crud

---
+++ app/Filament/Resources/ConfigResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ConfigResource\Pages;
+use App\Filament\Resources\ConfigResource\RelationManagers;
+use App\Models\Config;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ConfigResource extends Resource
+{
+    protected static ?string $model = Config::class;
+
+    protected static ?string $label = 'Настройку';
+    protected static ?string $navigationLabel = 'Настройки';
+    protected static ?string $pluralLabel = 'Настройки';
+
+    protected static ?string $breadcrumb = 'Настройки';
+
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('key')
+                    ->label('Ключ')
+                    ->columnSpanFull()
+                    ->required(),
+                TextInput::make('label')
+                    ->label('Название')
+                    ->columnSpanFull()
+                    ->required(),
+                TextInput::make('value')
+                    ->label('Значение')
+                    ->columnSpanFull()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('key')
+                    ->searchable()
+                    ->label('Ключ'),
+                TextColumn::make('label')
+                    ->searchable()
+                    ->label('Название'),
+                TextColumn::make('value')
+                    ->searchable()
+                    ->label('Значение')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListConfigs::route('/'),
+            'create' => Pages\CreateConfig::route('/create'),
+            'edit' => Pages\EditConfig::route('/{record}/edit'),
+        ];
+    }
+}
