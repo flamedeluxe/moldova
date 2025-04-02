0749f32f2f5b33617fd09cc01485bd8249942307
andreyfrost <flamedeluxe@yandex.ru>
2025-04-02 16:46:21 +0800

    add points for filled profile

--- app/Http/Controllers/Account/IndexController.php
+++ app/Http/Controllers/Account/IndexController.php
@@ -18,7 +18,7 @@ class IndexController extends Controller
     {
         $profile = Auth::user();
 
-        $validated = $request->validate([
+        $rules = [
             'surname' => 'required|string|max:100',
             'name' => 'required|string|max:100',
             'patronymic' => 'required|string|max:100',
@@ -26,13 +26,25 @@ class IndexController extends Controller
             'phone' => 'required|string|max:100',
             'email' => 'required|email|max:100',
             'socials' => 'array|nullable|max:5',
-        ]);
+        ];
+
+        $validated = $request->validate($rules);
 
         if(is_array($validated['socials'])) foreach($validated['socials'] as $idx => $item) {
             if($item == '') unset($validated['socials'][$idx]);
         }
 
         $profile->update($validated);
+
+        $count = 0;
+        foreach($rules as $field => $rule) {
+            if($profile->$field !== '') $count++;
+        }
+
+        if($count >= 6) {
+            $profile->bill += 350;
+            $profile->save();
+        }
 
         return response()->json([
             'success' => true,
