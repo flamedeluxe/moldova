2d962b6e431cd91e59660fd7ea3e69ffe46c7521
andreyfrost <flamedeluxe@yandex.ru>
2025-04-14 01:03:09 +0800

    feedbacks

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Mail\FeedbackMail;
+use App\Models\Feedback;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -10,14 +11,17 @@ class FormController extends BaseController
 {
     public function feedback(Request $request)
     {
-        $validated = $request->validate([
+        $request->validate([
             'name' => 'required|max:255',
             'phone' => 'required|min:11|max:20',
             'text' => 'required|max:500',
             'agree' => 'accepted',
         ]);
 
-        Mail::to(env('MAIL_TO'))->send(new FeedbackMail($validated));
+        $data = $request->except('_token', 'agree');
+        Feedback::query()->create($data);
+
+        Mail::to(env('MAIL_TO'))->send(new FeedbackMail($data));
 
         return response()->json([
             'success' => true,
