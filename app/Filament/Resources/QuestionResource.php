61b7f59dc9894051991bdb93818b67efd319520a
andreyfrost <flamedeluxe@yandex.ru>
2025-04-21 17:54:41 +0800

    blocks

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -4,6 +4,12 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\QuestionResource\Pages;
 use App\Models\Question;
+use Filament\Forms\Components\Builder;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -43,6 +49,156 @@ class QuestionResource extends Resource
                     ->rows(5)
                     ->required()
                     ->maxLength(255),
+                Builder::make('blocks')
+                    ->label('Блоки')
+                    ->collapsible()
+                    ->columnSpan('full')
+                    ->blocks([
+                        Builder\Block::make('heading')
+                            ->label('Заголовок')
+                            ->columns(1)
+                            ->schema([
+                                Select::make('level')
+                                    ->label('Уровень')
+                                    ->options([
+                                        'h1' => 'H1',
+                                        'h2' => 'H2',
+                                        'h3' => 'H3',
+                                        'h4' => 'H4',
+                                        'h5' => 'H5',
+                                        'h6' => 'H6',
+                                    ])
+                                    ->required(),
+                                TextInput::make('text')
+                                    ->label('Текст')
+                                    ->required(),
+                            ]),
+                        Builder\Block::make('paragraph')
+                            ->label('Параграф')
+                            ->schema([
+                                RichEditor::make('content')
+                                    ->label('Содержимое')
+                                    ->required(),
+                            ]),
+                        Builder\Block::make('image')
+                            ->label('Изображение')
+                            ->schema([
+                                FileUpload::make('image')
+                                    ->label('Фото')
+                                    ->image()
+                                    ->required(),
+
+                            ]),
+                        Builder\Block::make('gallery')
+                            ->label('Галерея')
+                            ->schema([
+                                FileUpload::make('gallery')
+                                    ->label('Галерея')
+                                    ->panelLayout('grid')
+                                    ->previewable(true)
+                                    ->reorderable(true)
+                                    ->multiple()
+                                    ->columnSpan('full'),
+                            ]),
+                        Builder\Block::make('block')
+                            ->label('Блок с фоном')
+                            ->schema([
+                                TextInput::make('title')
+                                    ->label('Заголовок')
+                                    ->required(),
+                                RichEditor::make('content')
+                                    ->label('Текст')
+                                    ->required(),
+                            ]),
+                        Builder\Block::make('blockquote')
+                            ->label('Цитата')
+                            ->schema([
+                                Textarea::make('content')
+                                    ->label('Текст')
+                                    ->rows(5)
+                                    ->required(),
+                                TextInput::make('caption')
+                                    ->label('Подпись'),
+                            ]),
+                        Builder\Block::make('image_left')
+                            ->label('Блок с фото слева')
+                            ->schema([
+                                Grid::make(2)
+                                    ->schema([
+                                        FileUpload::make('image')
+                                            ->label('Фото')
+                                            ->image()
+                                            ->required(),
+                                        RichEditor::make('content')
+                                            ->label('Текст')
+                                            ->required(),
+                                        TextInput::make('link')
+                                            ->label('Ссылка на фото'),
+                                    ]),
+                            ]),
+                        Builder\Block::make('image_right')
+                            ->label('Блок с фото справа')
+                            ->schema([
+                                Grid::make(2)
+                                    ->schema([
+                                        RichEditor::make('content')
+                                            ->label('Текст')
+                                            ->required(),
+                                        FileUpload::make('image')
+                                            ->label('Фото')
+                                            ->image()
+                                            ->required(),
+                                        TextInput::make('link')
+                                            ->label('Ссылка на фото'),
+                                    ]),
+                            ]),
+                        Builder\Block::make('cards')
+                            ->label('Карточки с подписями')
+                            ->schema([
+                                TextInput::make('title')
+                                    ->columnSpanFull()
+                                    ->label('Заголовок'),
+                                Grid::make(2)
+                                    ->schema([
+                                        Repeater::make('items')
+                                            ->label('Элементы')
+                                            ->columnSpanFull()
+                                            ->grid(3)
+                                            ->schema([
+                                                FileUpload::make('image')
+                                                    ->label('Фото')
+                                                    ->columnSpanFull()
+                                                    ->image(),
+                                                TextInput::make('title')
+                                                    ->columnSpanFull()
+                                                    ->label('Заголовок'),
+                                                TextInput::make('text')
+                                                    ->columnSpanFull()
+                                                    ->label('Текст'),
+                                            ]),
+                                    ]),
+                            ]),
+                        Builder\Block::make('logos')
+                            ->label('Карточки')
+                            ->schema([
+                                TextInput::make('title')
+                                    ->columnSpanFull()
+                                    ->label('Заголовок'),
+                                Grid::make(2)
+                                    ->schema([
+                                        Repeater::make('items')
+                                            ->label('Элементы')
+                                            ->columnSpanFull()
+                                            ->grid(4)
+                                            ->schema([
+                                                FileUpload::make('image')
+                                                    ->label('Фото')
+                                                    ->columnSpanFull()
+                                                    ->image(),
+                                            ]),
+                                    ]),
+                            ]),
+                    ])
             ]);
     }
 
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $casts = [
+        'blocks' => 'json',
+    ];
+    
     public function scopeActive($query)
     {
         return $query->where('active', 1);
